e6c7b242913c334790fec4921209c083260fdb37
Wassim <wassim.hasni42@gmail.com>
2019-05-19 23:29:51 +0200

    Debut de l'algo avec ft_resolve

--- srcs/ft_resolve.c
+++ srcs/ft_resolve.c
@@ -1,40 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_resolve.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wahasni <wahasni@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/03/18 04:14:48 by wahasni           #+#    #+#             */
-/*   Updated: 2019/05/19 23:27:34 by wahasni          ###   ########.fr       */
+/*   Created: 2019/05/19 23:28:44 by wahasni           #+#    #+#             */
+/*   Updated: 2019/05/19 23:29:26 by wahasni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/filler.h"
+#include "../includes/filler.h"
 
-void		ft_init(t_args *args)
+int	ft_resolve(t_args *args)
 {
-	args->line = NULL;
-	args->tab = NULL;
-}
-
-int 		main(void)
-{
-	t_args	args;
-	int		end;
-
-	end = 1;
-	ft_init(&args);
-	if (ft_init_player(&args, args.line))
-		return (1);
-	// printf("yess\n");
-	while (end)
-	{
-		if (ft_handle_map(&args))
-			return (1);
-		if (ft_handle_piece(&args))
-			return (1);
-		end = ft_resolve(&args);
-	}
-	return (0);
+	
 }
